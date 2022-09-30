d1177fd5846833b13c9a3b41f0f2a8603a0c6146
Davin Stirling <davin.stirling@capgemini.com>
2022-09-30 03:49:43 +0100

    tidied up main class and option to List All Accounts and Balances, and option to List Certain Account both work with larger csv file

--- transaction.py
+++ transaction.py
@@ -1,3 +1,4 @@
+from main import format_pence_as_currency
 class Transaction:
 
     def __init__(self, date, from_person, to_person, narrative, amount):
@@ -8,8 +9,8 @@ class Transaction:
         self.amount = amount    #should be ints as pence amount
 
     def __eq__(self, other):
-        print("_checking transaction obj equaltiy_")
+        print("_checking transaction obj equality_")
         return self.date == other.date and self.from_person == other.from_person and self.to_person == other.to_person and self.narrative == other.narrative and self.amount == other.amount
 
-    def printme(self):)
-        print(f"Date: {self.date},  Payment From: {self.from_person}, Payment To: {self.to_person}, Narrative: {self.narrative}, Amount: {self.amount}")
+    def printme(self):
+        print(f"Date: {self.date},  Payment From: {self.from_person}, Payment To: {self.to_person}, Narrative: {self.narrative}, Amount: {format_pence_as_currency(self.amount)}")
